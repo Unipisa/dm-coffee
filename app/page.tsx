60e2d5f37664ab0aa56a924b0ea0928869710884
Emanuele Paolini <emanuele.paolini@gmail.com>
2024-02-21 22:59:43 +0100

    init prisma

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,23 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col justify-between p-24">
       Home
+      <AddCoffee />
     </main>
   );
+}
+
+function AddCoffee() {
+  return <div>
+    <h1>Add Coffee</h1>
+    <form>
+      <select name="count">
+        <option value="1" selected>1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+      </select>
+      <button className="button" type="submit">Add</button>
+    </form>
+  </div>
 }
