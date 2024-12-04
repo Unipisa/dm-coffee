35b718584ebfd40a9a273ff9a05fbb7bdf42a7ae
Leonardo Robol <leo@robol.it>
2024-12-04 08:47:36 +0100

    table header styling

--- app/components/Thead.tsx
+++ app/components/Thead.tsx
@@ -1,5 +1,5 @@
 export default function Thead({children}:{ children: React.ReactNode }) {
-    return <thead className='border-t-2 border-gray-300 text-xs text-gray-700 uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400 text-align-left'>
+    return <thead className='border-y-2 border-gray-400 text-xs text-gray-700 uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400 text-align-left'>
         {children}
     </thead>
 }
