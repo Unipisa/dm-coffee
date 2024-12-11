8d4429197af3edfab9ba053ec4d8226dcccd239a
Leonardo Robol <leo@robol.it>
2024-12-11 19:07:07 +0100

    Ricarica!

--- app/components/CoffeeForm.tsx
+++ app/components/CoffeeForm.tsx
@@ -17,16 +17,38 @@ export default function CoffeeForm() {
 
   return <form>
     <div className="grid gap-6 my-6 md:grid-cols-1">
-      <div className='flex justify-center space-x-2'>
-        <Button variant='secondary' disabled={count <= 1} onClick={() => setCount(count => count - 1)}>
-          -
+      <div className='md:flex justify-between space-x-2'>
+        
+        <div className='my-4 hidden md:block'>
+        <Button>
+          <a className="no-underline text-white" href="https://bit.ly/DMcaffePaga">Ricarica: PayPal</a>
         </Button>
-      <Button onClick={() => submitCoffee({ variables: { count } })}>
-        Registra {count} caffè
-      </Button>
-      <Button variant='secondary' onClick={() => setCount(count => count + 1)}>
-          +
+        </div>
+        
+        <div className='flex space-x-2 my-4'>
+          <Button variant='secondary' disabled={count <= 1} onClick={() => setCount(count => count - 1)}>
+            -
+          </Button>
+          <Button onClick={() => submitCoffee({ variables: { count } })}>
+            Registra {count} caffè
+          </Button>
+          <Button variant='secondary' onClick={() => setCount(count => count + 1)}>
+              +
+          </Button>
+        </div>
+
+        <div className='mt-6 mb-1 md:hidden'>
+          Ricarica il tuo credito:
+        <Button>
+          <a className="no-underline text-white" href="https://bit.ly/DMcaffePaga">PayPal</a>
         </Button>
+        </div>
+
+        <div className='md:my-4'>
+        <Button>
+          <a className="no-underline text-white" href="https://bit.ly/DMcaffePagaSatispay"><span className='hidden md:inline'>Ricarica: </span>Satispay</a>
+        </Button>
+        </div>
       </div>
     </div>
   </form>
