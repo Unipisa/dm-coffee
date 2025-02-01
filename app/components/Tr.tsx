c6e20d570703eed5eea7ec95e0317c48510d72bd
Emanuele Paolini <emanuele.paolini@gmail.com>
2025-02-01 13:09:22 +0100

    fix: key is not passed to child

--- app/components/Tr.tsx
+++ app/components/Tr.tsx
@@ -1,5 +1,5 @@
-export default function Tr({children, className, key}:{ children: React.ReactNode, className?: string, key?: any }) {
-    return <tr key={key} className={"border-0 odd:bg-white odd:dark:bg-gray-900 even:bg-gray-100 even:dark:bg-gray-800 border-b dark:border-gray-700 " + className}>
+export default function Tr({children, className}:{ children: React.ReactNode, className?: string, key?: any }) {
+    return <tr className={"border-0 odd:bg-white odd:dark:bg-gray-900 even:bg-gray-100 even:dark:bg-gray-800 border-b dark:border-gray-700 " + className}>
         {children}
     </tr>
 }
